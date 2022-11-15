2a1e8a951bde5da25a10d57a08717b24c7e48d7a
galaxygit008 <prenish008@gmail.com>
2022-11-15 20:23:05 +0530

    otp resend done

--- model/auth.js
+++ model/auth.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose')
 const authSchema=new mongoose.Schema({
     email:String,
     otp:Number,
+    expireAt:Date
     // uid:String
     // expiry in 30 minutes
     // again if 30min passed and again try to signup then check for isVerified false so update otp at that time and check for 
